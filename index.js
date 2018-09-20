4436e13b614d5d2b1bba3eea1b93ffef0bdbf62a
Michael Chan <michael.chan212@gmail.com>
2018-09-20 19:04:08 +0000

    Done.

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ function increaseRankBy(n) {
 function deepestChild() {
   var divs = document.querySelectorAll('#grand-node div');
   var deepest = divs[divs.length-1];
+<<<<<<< HEAD
 
+=======
+  
+>>>>>>> dc4f7b1716afeb7c93d0549b0e55a392d87e2761
   return deepest;
 }
