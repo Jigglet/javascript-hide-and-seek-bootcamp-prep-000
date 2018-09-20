20d1b7a981c670514438267e7529d0429a9f49e4
Michael Chan <michael.chan212@gmail.com>
2018-09-20 02:54:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+ function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var inc = document.querySelectorAll('.ranked-list li');
+  for (var i = 0; i < inc.length; i++) {
+    inc[i].innerHTML = parseInt(inc[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  var divs = document.querySelectorAll('#grand-node div');
+  var deepest = divs[divs.length-1];
+  return deepest;
+}
